99aa36e30222a06e52e5fa81fa2efbf4b6187d37
samurodriguez <samuelrodriguezrey@outlook.com>
2020-11-29 21:35:18 +0100

    Subida db.sql

---
+++ db/db.sql
@@ -0,0 +1,322 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: esports_talents
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aficionado`
+--
+
+DROP TABLE IF EXISTS `aficionado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aficionado` (
+  `id_usuario` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  CONSTRAINT `aficionado_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aficionado`
+--
+
+LOCK TABLES `aficionado` WRITE;
+/*!40000 ALTER TABLE `aficionado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aficionado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipo`
+--
+
+DROP TABLE IF EXISTS `equipo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipo` (
+  `id_equipo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `f_fundacion` date DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `provincia` varchar(40) DEFAULT NULL,
+  `tlf` char(9) DEFAULT NULL,
+  PRIMARY KEY (`id_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipo`
+--
+
+LOCK TABLES `equipo` WRITE;
+/*!40000 ALTER TABLE `equipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jugador`
+--
+
+DROP TABLE IF EXISTS `jugador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jugador` (
+  `id_usuario` int NOT NULL,
+  `id_equipo` int DEFAULT NULL,
+  `posicion` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_equipo` (`id_equipo`),
+  CONSTRAINT `jugador_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `jugador_ibfk_2` FOREIGN KEY (`id_equipo`) REFERENCES `equipo` (`id_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jugador`
+--
+
+LOCK TABLES `jugador` WRITE;
+/*!40000 ALTER TABLE `jugador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jugador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ojeador`
+--
+
+DROP TABLE IF EXISTS `ojeador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ojeador` (
+  `id_usuario` int NOT NULL,
+  `id_equipo` int DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_equipo` (`id_equipo`),
+  CONSTRAINT `ojeador_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `ojeador_ibfk_2` FOREIGN KEY (`id_equipo`) REFERENCES `equipo` (`id_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ojeador`
+--
+
+LOCK TABLES `ojeador` WRITE;
+/*!40000 ALTER TABLE `ojeador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ojeador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id_post` int NOT NULL AUTO_INCREMENT,
+  `texto` varchar(500) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seguidores`
+--
+
+DROP TABLE IF EXISTS `seguidores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seguidores` (
+  `id_usuario` int NOT NULL,
+  `id_seguidor` int NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_seguidor`),
+  KEY `id_seguidor` (`id_seguidor`),
+  CONSTRAINT `seguidores_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `seguidores_ibfk_2` FOREIGN KEY (`id_seguidor`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seguidores`
+--
+
+LOCK TABLES `seguidores` WRITE;
+/*!40000 ALTER TABLE `seguidores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seguidores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitud_contratacion`
+--
+
+DROP TABLE IF EXISTS `solicitud_contratacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitud_contratacion` (
+  `id_ojeador` int NOT NULL,
+  `id_jugador` int NOT NULL,
+  `f_solicitud` date NOT NULL,
+  `asunto` varchar(255) DEFAULT NULL,
+  `mensaje` varchar(500) NOT NULL,
+  PRIMARY KEY (`id_ojeador`,`id_jugador`),
+  KEY `id_jugador` (`id_jugador`),
+  CONSTRAINT `solicitud_contratacion_ibfk_1` FOREIGN KEY (`id_ojeador`) REFERENCES `ojeador` (`id_usuario`),
+  CONSTRAINT `solicitud_contratacion_ibfk_2` FOREIGN KEY (`id_jugador`) REFERENCES `jugador` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitud_contratacion`
+--
+
+LOCK TABLES `solicitud_contratacion` WRITE;
+/*!40000 ALTER TABLE `solicitud_contratacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitud_contratacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_comment`
+--
+
+DROP TABLE IF EXISTS `user_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_comment` (
+  `id_usuario` int NOT NULL,
+  `id_post` int NOT NULL,
+  `comment_text` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_post`),
+  KEY `id_post` (`id_post`),
+  CONSTRAINT `user_comment_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `user_comment_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_comment`
+--
+
+LOCK TABLES `user_comment` WRITE;
+/*!40000 ALTER TABLE `user_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_like`
+--
+
+DROP TABLE IF EXISTS `user_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_like` (
+  `id_usuario` int NOT NULL,
+  `id_post` int NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_post`),
+  KEY `id_post` (`id_post`),
+  CONSTRAINT `user_like_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `user_like_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_like`
+--
+
+LOCK TABLES `user_like` WRITE;
+/*!40000 ALTER TABLE `user_like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_like` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_share`
+--
+
+DROP TABLE IF EXISTS `user_share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_share` (
+  `id_usuario` int NOT NULL,
+  `id_post` int NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_post`),
+  KEY `id_post` (`id_post`),
+  CONSTRAINT `user_share_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `user_share_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_share`
+--
+
+LOCK TABLES `user_share` WRITE;
+/*!40000 ALTER TABLE `user_share` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_share` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `nickname` varchar(40) DEFAULT NULL,
+  `usr_password` varchar(255) NOT NULL,
+  `f_nacimiento` date DEFAULT NULL,
+  `tlf` char(9) DEFAULT NULL,
+  `provincia` varchar(40) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `bio` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-29 21:18:45
